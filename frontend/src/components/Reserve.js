5e94585911d0eb3b36f7c0cf752bea3c9067ab8c
vini-afonso <vini.afonsodev@gmail.com>
2024-09-21 22:29:04 -0300

    Uptade Reserve.js

--- frontend/src/components/Reserve.js
+++ frontend/src/components/Reserve.js
@@ -1,20 +1,21 @@
 // src/components/Reserve.js
-import React, { useState } from 'react'
-import './Reserve.css'
-import './ReservationModal.css' // Importar estilos do modal
+import React, { useState } from 'react';
+import axios from 'axios'; // Importar axios
+import './Reserve.css';
+import './ReservationModal.css'; // Importar estilos do modal
 
 const ReservationModal = ({ isOpen, onClose, onSubmit, quadra }) => {
-  const [nome, setNome] = useState('')
-  const [qtdePessoas, setQtdePessoas] = useState('')
-  const [telefone, setTelefone] = useState('')
+  const [nome, setNome] = useState('');
+  const [qtdePessoas, setQtdePessoas] = useState('');
+  const [telefone, setTelefone] = useState('');
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    onSubmit({ nome, qtdePessoas, telefone, quadra }) // Inclui a quadra nos dados da reserva
-    onClose() // Fecha o modal após o envio
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ nome, qtdePessoas, telefone, quadra }); // Inclui a quadra nos dados da reserva
+    onClose(); // Fecha o modal após o envio
+  };
 
-  if (!isOpen) return null
+  if (!isOpen) return null;
 
   return (
     <div className="modal-overlay">
@@ -26,7 +27,7 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, quadra }) => {
             <input
               type="text"
               value={nome}
-              onChange={e => setNome(e.target.value)}
+              onChange={(e) => setNome(e.target.value)}
               required
             />
           </label>
@@ -35,7 +36,7 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, quadra }) => {
             <input
               type="number"
               value={qtdePessoas}
-              onChange={e => setQtdePessoas(e.target.value)}
+              onChange={(e) => setQtdePessoas(e.target.value)}
               required
             />
           </label>
@@ -44,7 +45,7 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, quadra }) => {
             <input
               type="tel"
               value={telefone}
-              onChange={e => setTelefone(e.target.value)}
+              onChange={(e) => setTelefone(e.target.value)}
               required
             />
           </label>
@@ -55,143 +56,70 @@ const ReservationModal = ({ isOpen, onClose, onSubmit, quadra }) => {
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const Reserve = () => {
-  const [selectedDate, setSelectedDate] = useState('26/08') // Data padrão
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const [selectedQuadra, setSelectedQuadra] = useState('') // Para armazenar a quadra selecionada
+  const [selectedDate, setSelectedDate] = useState('26/08'); // Data padrão
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedQuadra, setSelectedQuadra] = useState(''); // Para armazenar a quadra selecionada
   const reservasData = {
     '25/08': [
-      {
-        data: '25/08 - 07:00 às 08:00',
-        valor: 'R$ 200,00',
-        piso: 'Areia',
-        coberta: 'Não',
-        quadra: 'Quadra 01'
-      },
-      {
-        data: '25/08 - 07:00 às 08:00',
-        valor: 'R$ 200,00',
-        piso: 'Areia',
-        coberta: 'Não',
-        quadra: 'Quadra 02'
-      },
-      {
-        data: '25/08 - 07:00 às 08:00',
-        valor: 'R$ 200,00',
-        piso: 'Areia',
-        coberta: 'Não',
-        quadra: 'Quadra 03'
-      },
-      {
-        data: '25/08 - 07:00 às 08:00',
-        valor: 'R$ 200,00',
-        piso: 'Areia',
-        coberta: 'Não',
-        quadra: 'Quadra 04'
-      },
-      {
-        data: '25/08 - 07:00 às 08:00',
-        valor: 'R$ 200,00',
-        piso: 'Areia',
-        coberta: 'Não',
-        quadra: 'Quadra 05'
-      }
+      { data: '25/08 - 07:00 às 08:00', valor: 'R$ 200,00', piso: 'Areia', coberta: 'Não', quadra: 'Quadra 01' },
+      { data: '25/08 - 07:00 às 08:00', valor: 'R$ 200,00', piso: 'Areia', coberta: 'Não', quadra: 'Quadra 02' },
+      { data: '25/08 - 07:00 às 08:00', valor: 'R$ 200,00', piso: 'Areia', coberta: 'Não', quadra: 'Quadra 03' },
+      { data: '25/08 - 07:00 às 08:00', valor: 'R$ 200,00', piso: 'Areia', coberta: 'Não', quadra: 'Quadra 04' },
+      { data: '25/08 - 07:00 às 08:00', valor: 'R$ 200,00', piso: 'Areia', coberta: 'Não', quadra: 'Quadra 05' }
     ],
     '26/08': [
-      {
-        data: '26/08 - 07:00 às 08:00',
-        valor: 'R$ 200,00',
-        piso: 'Areia',
-        coberta: 'Não',
-        quadra: 'Quadra 01'
-      },
-      {
-        data: '26/08 - 07:00 às 08:00',
-        valor: 'R$ 200,00',
-        piso: 'Areia',
-        coberta: 'Não',
-        quadra: 'Quadra 02'
-      },
-      {
-        data: '26/08 - 07:00 às 08:00',
-        valor: 'R$ 200,00',
-        piso: 'Areia',
-        coberta: 'Não',
-        quadra: 'Quadra 03'
-      },
-      {
-        data: '26/08 - 07:00 às 08:00',
-        valor: 'R$ 200,00',
-        piso: 'Areia',
-        coberta: 'Não',
-        quadra: 'Quadra 04'
-      },
-      {
-        data: '26/08 - 07:00 às 08:00',
-        valor: 'R$ 200,00',
-        piso: 'Areia',
-        coberta: 'Não',
-        quadra: 'Quadra 05'
-      }
+      { data: '26/08 - 07:00 às 08:00', valor: 'R$ 200,00', piso: 'Areia', coberta: 'Não', quadra: 'Quadra 01' },
+      { data: '26/08 - 07:00 às 08:00', valor: 'R$ 200,00', piso: 'Areia', coberta: 'Não', quadra: 'Quadra 02' },
+      { data: '26/08 - 07:00 às 08:00', valor: 'R$ 200,00', piso: 'Areia', coberta: 'Não', quadra: 'Quadra 03' },
+      { data: '26/08 - 07:00 às 08:00', valor: 'R$ 200,00', piso: 'Areia', coberta: 'Não', quadra: 'Quadra 04' },
+      { data: '26/08 - 07:00 às 08:00', valor: 'R$ 200,00', piso: 'Areia', coberta: 'Não', quadra: 'Quadra 05' }
     ],
     '27/08': [
-      {
-        data: '27/08 - 07:00 às 08:00',
-        valor: 'R$ 200,00',
-        piso: 'Areia',
-        coberta: 'Não',
-        quadra: 'Quadra 01'
-      }
+      { data: '27/08 - 07:00 às 08:00', valor: 'R$ 200,00', piso: 'Areia', coberta: 'Não', quadra: 'Quadra 01' }
     ]
-  }
+  };
 
-  const handleDateClick = date => {
-    setSelectedDate(date)
-  }
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+  };
 
-  const handleReserveClick = quadra => {
-    setSelectedQuadra(quadra) // Armazena a quadra selecionada
-    setIsModalOpen(true)
-  }
+  const handleReserveClick = (quadra) => {
+    setSelectedQuadra(quadra); // Armazena a quadra selecionada
+    setIsModalOpen(true);
+  };
 
-  const handleModalSubmit = data => {
-    console.log('Dados da Reserva:', { ...data, quadra: selectedQuadra })
-    // Aqui você pode adicionar lógica para salvar a reserva
-    setIsModalOpen(false) // Fecha o modal após o envio
-  }
+  const handleModalSubmit = async (data) => {
+    const reservaData = { ...data, quadra: selectedQuadra };
+
+    try {
+      const response = await axios.post('http://localhost:3000/reservas', reservaData); // Altere a URL para seu endpoint
+      console.log('Reserva salva com sucesso:', response.data);
+    } catch (error) {
+      console.error('Erro ao salvar reserva:', error);
+    }
+
+    setIsModalOpen(false); // Fecha o modal após o envio
+  };
 
   return (
     <div className="reserve-container">
       <h1>Reserve</h1>
       <h2>Quadra</h2>
       <div className="dates">
-        {['25/08', '26/08', '27/08', '28/08', '29/08', '30/08', '31/08'].map(
-          (date, index) => (
-            <button
-              key={index}
-              className={`date-button ${selectedDate === date ? 'active' : ''}`}
-              onClick={() => handleDateClick(date)}
-            >
-              {date}{' '}
-              {index === 0
-                ? '(Domingo)'
-                : index === 1
-                ? '(Segunda)'
-                : index === 2
-                ? '(Terça)'
-                : index === 3
-                ? '(Quarta)'
-                : index === 4
-                ? '(Quinta)'
-                : index === 5
-                ? '(Sexta)'
-                : '(Sábado)'}
-            </button>
-          )
-        )}
+        {['25/08', '26/08', '27/08', '28/08', '29/08', '30/08', '31/08'].map((date, index) => (
+          <button
+            key={index}
+            className={`date-button ${selectedDate === date ? 'active' : ''}`}
+            onClick={() => handleDateClick(date)}
+          >
+            {date}{' '}
+            {index === 0 ? '(Domingo)' : index === 1 ? '(Segunda)' : index === 2 ? '(Terça)' : index === 3 ? '(Quarta)' : index === 4 ? '(Quinta)' : index === 5 ? '(Sexta)' : '(Sábado)'}
+          </button>
+        ))}
       </div>
       <table>
         <thead>
@@ -232,7 +160,7 @@ const Reserve = () => {
         quadra={selectedQuadra} // Passa a quadra selecionada para o modal
       />
     </div>
-  )
-}
+  );
+};
 
-export default Reserve
+export default Reserve;
