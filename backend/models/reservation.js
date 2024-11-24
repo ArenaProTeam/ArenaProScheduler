1dcbf34db77329f012244457e2e74efd1ea9cb9a
vini-afonso <vini.afonsodev@gmail.com>
2024-11-23 23:29:51 -0300

    Reserv

--- backend/models/reservation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ReservationSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  arena: { type: String, required: true },
-  date: { type: Date, required: true },
-});
-
-module.exports = mongoose.model('Reservation', ReservationSchema);
